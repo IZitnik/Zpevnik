c30d1f4acef09194d9a40e0b508b0fe9300d88ee
Ivo Zitnik <zitnik.ivo@gmail.com>
2025-07-29 17:58:42 +0200

    Generating songbook based on source file

--- configure.py
+++ configure.py
@@ -5,16 +5,24 @@ RES_DIR = "result"
 CONFIG = "config.json"
 COVER = "cover.pdf"
 SONGBOOK = "Zpevnik.pdf"
-
-# Get all .chordpro files in the songs directory
-songs = [os.path.join(SONG_DIR, f)
-         for f in os.listdir(SONG_DIR) if f.endswith(".chordpro")]
+SONGBOOK_SRC = "songbook.txt"
 
 # initialize RES_DIR & SONG_DIR
 if not os.path.exists(SONG_DIR):
     os.makedirs(SONG_DIR)
 if not os.path.exists(RES_DIR):
     os.makedirs(RES_DIR)
+
+# Get all .chordpro files in the songs directory
+songs = [os.path.join(SONG_DIR, f)
+         for f in os.listdir(SONG_DIR) if f.endswith(".chordpro")]
+
+# Generate songbook.txt if it doesn't exist
+if not os.path.exists(SONGBOOK_SRC):
+    with open(SONGBOOK_SRC, "w") as f:
+        f.write(COVER + "\n")
+        for song in songs:
+            f.write(song + "\n")
 
 with open("build.ninja", "w") as f:
     f.write(f"""# Auto-generated build.ninja
@@ -25,12 +33,14 @@ cover = {COVER}
 songbook = {SONGBOOK}
 
 rule build_songbook
-  command = chordpro --config=$config --cover=$cover $in -o $res_dir/$out
-  description = Building songbook $out
+  command = chordpro --config=$config \
+                     --file=$in \
+                     -o $res_dir/$out
+  description = Building songbook $out according to $in
 
 rule build_song
   command = chordpro --config=$config $in -o $res_dir/$out
-  description = Building song $in
+  description = Building song $out
 
 rule clean
   command = rm -rf $res_dir/*
@@ -40,12 +50,12 @@ rule clean
 
     # Songbook build using all songs
     song_inputs = " ".join(songs)
-    f.write(f"build $songbook: build_songbook {song_inputs}\n\n")
-
+    f.write(f"build $songbook: build_songbook {SONGBOOK_SRC}\n\n")
     # Per-song builds
     for song in songs:
         f.write(f"build {os.path.basename(song)[
                 :-9]}.pdf: build_song {song}\n")
-
     f.write("\n")
     f.write("build clean: clean\n")
+    f.write("\n")
+    f.write(f"default {SONGBOOK}\n")
